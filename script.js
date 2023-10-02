ba1ae7d95af44bdca6db8dc6f6fd51d0e617563a
Chris <97418708+ChrisMcGill@users.noreply.github.com>
2023-10-02 18:57:20 +0100

    notes on logic, divs not forming

--- script.js
+++ script.js
@@ -1,24 +1,35 @@
+// grid container needs to be a set height and width
+// the pixels in etch can be a percentage of this worked
+// out by the gridsize 
+// so pixelsize = height/(gridsize+border) 
 
 
-
+let gridSize = 4;
+let colorConst; //set the number that can be used as an RGB or HEX value
 
 //functions for each button
 
 //input to change grid size (square or input choice?)s
 function changeGridSize(){
-
+    for (let i = 0; i < (gridSize * gridSize); i++) {
+        let makeDiv = document.createElement('div');
+        makeDiv.className = 'gridDiv';
+    };
 };
+
 //change colour fo sketch
 function changeColour(){
-
+    //chane color of colorconst
 };
 //clear grid to blank canvas
 function resetGrid(){
-
+    //set colorconst to white
+    // change all divs to colorconst
 };
 //draw on hover
 function draw(){
-
+    //set colorconst to black
+    //set background color of griddiv on hover 
 };
 
 //grid manipulation
@@ -38,6 +49,6 @@ resetBtn,addEventListener('click', () => {
 });
 
 
-
 //need to set up default grid on load 16x16
-const defaultGrid = document.querySelector('#gridcontainer');
+const newGrid = document.querySelector('#gridContainer');
+changeGridSize(newGrid);
